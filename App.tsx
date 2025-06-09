cacd290610ed1fb5164266cf4d49b35d83882d80
Mahdi <mahdi.mousavi0011@gmail.com>
2025-06-09 15:33:51 +0200

    add layout.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Layout from './Layout/Layout';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      {/* <Text>Hello Mahdi!
+        Slutprjekt react native! hello!
+      </Text>
+      <StatusBar style="auto" /> */}
+      <Layout children={undefined}/>
     </View>
   );
 }
@@ -13,8 +17,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ Layout/Layout.tsx
@@ -0,0 +1,44 @@
+/** @format */
+
+import React from "react";
+import { View, Text, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import Footer from "../Components/footer";
+import Header from "../Components/header";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <SafeAreaView style={styles.safeView}>
+        <Header/>
+        <ScrollView contentContainerStyle={styles.body}>
+          <Text style={styles.title}>body</Text>
+          {children}
+        </ScrollView>
+        <Footer />
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeView: {
+    backgroundColor: "skyblue",
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "rgb(234, 214, 179)",
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 15,
+    paddingBottom: 80,
+  },
+});
