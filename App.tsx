53e596bc3e3ea906dc1c808d83db10df462fc4d3
Mahdi <mahdi.mousavi0011@gmail.com>
2025-06-16 01:33:17 +0200

    add favorites provider to app.

--- App.tsx
+++ App.tsx
@@ -2,14 +2,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./Navigation/Navigation";
 import { StatusBar } from "react-native";
+import { FavoritesProvider } from "./FavoritesContext/FavoritesContext";
 
 export default function App() {
   return (
     <>
+    <FavoritesProvider>
       <NavigationContainer>
         <Navigation />
       </NavigationContainer>
       <StatusBar barStyle="default" />
+    </FavoritesProvider>
     </>
   );
 }
